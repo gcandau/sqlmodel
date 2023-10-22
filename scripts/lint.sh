b2e7db51b1b775d0f18714f12fd7a3819c802a9d
gcandau <guillaume.candau@klocel.com>
2023-10-22 09:17:01 +0200

    Update lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-mypy sqlmodel_v2_beta
+# mypy sqlmodel_v2_beta
 flake8 sqlmodel_v2_beta tests docs_src
 black sqlmodel_v2_beta tests docs_src --check
 isort sqlmodel_v2_beta tests docs_src scripts --check-only
